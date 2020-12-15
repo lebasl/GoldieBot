a7ab366443c7eab11379b19ce3f2246b1cad4472
lebasl <43446196+lebasl@users.noreply.github.com>
2020-12-15 20:42:15 +0000

    Seems functional lacks improvement
    despair

--- GUI.py
+++ GUI.py
@@ -20,38 +20,53 @@ backimg = Image.open(".\\GUI_Materials\\backgrounds.jpg")
 img_fundo = Image.open(".\\GUI_Materials\\logo.gif")
 
 global inity
+global fail
+global reply
 
 greetings = ['ola', 'oi', 'boas', 'hey', 'viva', 'saudacoes',
-                 'cumprimentos']
+                 'cumprimentos', 'bom dia', 'boa tarde',
+                 'boa noite']
 thanks = ['de nada', 'ora essa', 'o prazer foi todo meu', 'sem problema',
               'estou aqui para ajudar']
 
 
 def thread_NLP_handler(arg):
-     values = preprocess_msg(arg[0],arg[1])
-     # print(values)
-     greeting = values[0]
-     th = values[1]
-     
-     if greeting:
-         txt = random.choice(greetings)
-         add_new_msg_chat(canvas,1, txt)
-     if th:
-         txt = random.choice(thanks)
-         add_new_msg_chat(canvas,1, txt)
-     thread = Thread(target=thread_IR_handler,args=(values[2:],))
-     thread.start() 
-     
+
+    values = preprocess_msg(arg)
+    #print(values)
+    greeting = values[0]
+    th = values[1]
+    reply = 0
+    global fail
+
+    if greeting:
+        txt = random.choice(greetings)
+        add_new_msg_chat(canvas,1, txt)
+        reply = 1
+    if th:
+        txt = random.choice(thanks)
+        add_new_msg_chat(canvas,1, txt)
+        reply = 1
+    thread = Thread(target=thread_IR_handler,args=(values[2:],))
+    thread.start()
+    thread.join(5)
+
+    if fail == 1 and reply == 0:
+        add_new_msg_chat(canvas, 1, "Desculpa, nao percebi!! Vamos fazer compras?")
+        fail = 0
+
 def thread_IR_handler(arg):
-    
-    # print(arg)
+    global fail
+    print(arg)
     msg = detect(arg[0],arg[1])
-    if(msg !=None):
+    if(msg != None):
         add_new_msg_chat(canvas,1, msg)
-        
-    
+        fail = 0
+    else:
+        fail = 1
+
 def setGUI():
-    
+
     root = Tk()
 
     #Load all Materials needed
@@ -64,79 +79,77 @@ def setGUI():
 
     file_menu = Menu(root,tearoff=False)
     main_menu = Menu(root)
-    
+
     file_menu.add(label="Save Chat Log",command=lambda: print("Hello"),itemType="command")
     file_menu.add(label="About",command=lambda: print("Hello"),itemType="command")
     file_menu.add(label="Exit",command=lambda: print("Hello"),itemType="command")
-    
+
     main_menu.add_cascade(label="File",menu=file_menu)
     root.config(menu=main_menu)
-    
+
     w = img.width()
     h = img.height()
-    
+
     root.geometry('%dx%d+0+0' % (w,h))
-    
+
     #Set Background
     labelimg = Label(root,image=img)
     labelimg.place(x=0,y=0,relwidth=1,relheight=1)
-    
-    
-    
+
     return root
 
 def create_chat_canvas(root):
     #Create Chat Window(CANVAS)
     canvas = Canvas(root,bg='#E5F3F2',bd=1,width=25, height=5)
     canvas.place(x=304,y=30,height=390,width=330)
-    
+
     #Scrollbar
     vbar = Scrollbar(canvas,orient=VERTICAL)
     vbar.config(command=canvas.yview)
     # vbar.pack(side=RIGHT,fill=Y)
-    
+
     canvas.config(yscrollcommand=vbar.set)
 
-    
+
     return canvas
 
 
 def create_input_window(root):
-    
+
     inwin = Text(root,bd=1,bg='#EAF8F7',width=30, height=4)
     inwin.place(x=290,y=430,height=40,width=300)
     inwin.insert(1.0,'Escreva aqui...')
-    
+
     inwin.bind("<Button-1>",input_entry)
-    
+
     return inwin
 
 def add_new_msg_chat(canvas,who,content):
-   
-    global inity 
+
+    global inity
     content = content
     myFont = font.Font(size=10,family='Ubuntu Condensed')
 
     if who==1:
         msg = "GoldieBot:\n"
-        final = msg + content
+        final = msg + str(content)
         txt = canvas.create_text(10,inity,font=myFont,justify="left",fill="black",text=final,anchor="nw",width=300)
         bbox = canvas.bbox(txt)
         rect_item = canvas.create_rectangle(bbox,width=2, outline="#CDB720", fill="#F4EDBA")
         canvas.tag_raise(txt,rect_item)
-        print(bbox)
-        inity = inity + (bbox[3]-bbox[1] +10)
 
-        
-    elif who==2:    
+        inity = inity + 45
+
+
+    elif who==2:
         msg = "Utilizador:\n"
-        final = msg+content
+        final = msg+ str(content)
         txt = canvas.create_text(300,inity,justify="left",font=myFont,fill="black",text=final,anchor="ne")
         bbox = canvas.bbox(txt)
         rect_item = canvas.create_rectangle(bbox,width=2, outline="#4DB823", fill="#AFF991")
         canvas.tag_raise(txt,rect_item)
-        inity = inity + (bbox[3]-bbox[1]+10)
-    
+        inity = inity + 45
+
     canvas.update_idletasks()
     canvas.config(scrollregion=bbox)
 
@@ -149,58 +162,55 @@ def input_entry(event):
         aux.delete(1.0,"end-1c")
         aux.insert(1.0,"")
     # print(txt)
-    # inwin.delete(1.0,"end")  
-    
+    # inwin.delete(1.0,"end")
+
 def btn_handler():
     txt = inwin.get("1.0","end-1c")
     add_new_msg_chat(canvas,2, txt)
     inwin.delete(1.0,END)
-    aux = []
-    aux.append(txt)
-    aux.append(0)
+
     #Handle NLP pre-procesing
-    thread = Thread(target=thread_NLP_handler,args=(aux,))
+    thread = Thread(target=thread_NLP_handler,args=(txt,))
     thread.start()
-       
+
 
 if __name__ == '__main__':
-    
+
     inity=30
-    
+
     root = setGUI()
-    
-    
+
+
     # c.config(font=("Times New Roman",20))
     # creditos = Text(root,height=2,width=30)
     # creditos.pack()
-  
+
     # c.pack()
-    
+
     img = ImageTk.PhotoImage(backimg)
     labelimg = Label(root,image=img)
     labelimg.place(x=0,y=0,relwidth=1,relheight=1)
-    
+
     img2 = ImageTk.PhotoImage(img_fundo)
     #Creates canvas(chatwindow) and Input (inwindow)
     canvas = create_chat_canvas(root)
-    
+
     canvas.create_image(160,200,image=img2,)
-    
+
     # canvas.pack()
     #Input window
     inwin = create_input_window(root)
 
     add_new_msg_chat(canvas,1,"Bem-vindo(a), em que lhe posso ajudar?")
-   
+
     #Create Send Button
     myFont = font.Font(size=30)
-   
+
     send_btn = Button(root,command=btn_handler,text=">",bg='#6495ED',activebackground='#EAF8F7',width=12,height=5)
     send_btn.place(x=595,y=430,height=42,width=60)
     send_btn['font'] = myFont
     # send_btn.bind("<Button-1>",btn_handler(canvas, inity))
-    
+
     root.mainloop()
-    
+
     root.quit()
-  
--- NLPreprocessor.py
+++ NLPreprocessor.py
@@ -4,8 +4,7 @@ Created on Mon Nov 30 01:20:50 2020
 
 @author: Eduardo Vicente and Isabel Carvalho
 """
-from wordspell_corrector import *
-from NLPyPort.FullPipeline import *
+
 import pandas as pd
 import string
 from nltk.corpus import stopwords
@@ -14,11 +13,6 @@ import spacy
 import nltk
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.corpus import floresta
-import joblib
-from nltk import word_tokenize
-
-question_words = ['que','quanto','quantos','cujo','cujos','quem','onde','quando','como','por','qual','quais','para','em']
-
 
 
 def find_greeting(words):
@@ -50,15 +44,13 @@ def find_thanks(words):
     return thank,idx
 
 
-
 def remove_noise(words):
     punctuation = string.punctuation
     white_space = string.whitespace
-    endoffile = 'EOS'
     filtered = []
 
     for w in words:
-        if w is not endoffile and w not in punctuation and w not in white_space:
+        if w not in punctuation and w not in white_space:
             filtered.append(w)
 
     return filtered
@@ -66,39 +58,34 @@ def remove_noise(words):
 
 def remove_stopwords(words):
     filtered = []
-    # print("STOP:\n")
-    # print(words)
     stop_words = set(stopwords.words('portuguese'))
 #    print(stop_words)
 
     for w in words:
-        # print(w+"\n")
-        if w.lower() not in question_words:
-            if w not in stop_words:
-                filtered.append(w)
-        else:
+        if w not in stop_words:
             filtered.append(w)
 
     return filtered
 
 
+def remove_all(words):
+    filtered = remove_noise(words)
+    filtered = remove_stopwords(words)
+    return filtered
 
 
 def part_of_speech(words,nlp):
-    
-    
     pos_tag = []
-    
+
     # using spacy pt
-    
-    
-    for w in words:  
-        
+
+    for w in words:
+
         for token in nlp(w):
             #valor a nivel semantico das palavras
             pos = token.pos_
             pos_tag.append(pos)
-            
+
     return pos_tag
 
 
@@ -106,14 +93,14 @@ def stemming_lemmatization(words,nlp):
     filtered = []
 
     for w in words:
-               
+
         for token in nlp(w):
-            # encontra root de cada palavra 
+            # encontra root de cada palavra
             w = token.lemma_
             filtered.append(w)
-           
-    
+
     return filtered
+
 
 def bag_of_words(words):
     vectorizer = CountVectorizer()
@@ -125,75 +112,45 @@ def bag_of_words(words):
     # print(df)
 
     return df
-def autocorrect(tokens):
-    arr = []
-    print(tokens)
-    for t in tokens:
-        if t.isalpha() and t != "EOS":
-            arr.append(correction(t))
-    print(arr)
-    return arr
-    
-def NLPyPort_transform(msg):
-    
-    options = {
-			"tokenizer" : True,
-			"pos_tagger" : True,
-			"lemmatizer" : True,
-			"entity_recognition" : True,
-			"np_chunking" : True,
-			"pre_load" : False,
-			"string_or_array" : True
-    }
-    text = new_full_pipe(msg,options=options)
-    dic_pos = dict()
-    tags = text.pos_tags
-    
-    
-    #SAVE POS_TAGS POSITIONS
-    for i in range(len(tags)):
-        dic_pos[text.lemas[i]] = tags[i]
 
-    
-    #PRINT PARAMETERS FROM TEXT:
-        
-    # if(text!=0):
-    #   print("TOKENS: "+ str(text.tokens)+" \n")
-    #   print("POS_TAG: "+ str(text.pos_tags)+" \n")
-    #   print("ENTITIES: "+ str(text.entities)+" \n")
-    #   print("NP_TAGS: "+ str(text.np_tags)+" \n")
 
-    filtered = autocorrect(text.tokens)
-    
-    #FIND GREETING WORDS ->
-    [greeting,idx_g] = find_greeting(filtered)
-   
-    #FIND THANKFULL WORDS ->
-    [thanks,idx_t] = find_thanks(filtered)
-    
-    filtered = text.lemas
-  
-    #REMOVE PONCTUATION NOISE and EOS
-    filtered = remove_noise(filtered)
-    #REMOVE STOPWORDS
-    filtered = remove_stopwords(filtered)
-    arr_tag = []
-    for i in range(len(filtered)):
-        if filtered[i] in list(dic_pos.keys()):
-            arr_tag.append(dic_pos[filtered[i]])
- 
-    return [greeting,thanks,filtered,arr_tag]
+def translate(words):
+    acentos = ["padrão", "preço", "recomendações", "decoração",
+               "classificação", "estação", "médio", "pétala", "tranças",
+               "acrílico", "botão", "geométrico", "pendão", "caída",
+               "algodão", "único", "pólo", "império", "íntimo", "boémia",
+               "médio-alto", "verão", "laço", "coração", "botões",
+               "alças", "três", "lã", "poliéster", "sintético", "pérolas",
+               "sólido", "impressão", "calças", "calções"]
+    traducao = ["padrao", "preco", "recomendacoes", "decoracao",
+                "classificacao", "estacao", "medio", "petala", "trancas",
+                "acrilico", "botao", "geometrico", "pendao", "caida",
+                "algodao", "unico", "polo", "imperio", "intimo", "boemia",
+                "medio-alto", "verao", "laco", "coracao", "botoes",
+                "alcas", "tres", "la", "poliester", "sintetico", "perolas",
+                "solido", "impressao", "calcas", "calcoes"]
+    filtered = []
 
-def Spacy_NLP_transform(msg):
-    
-      
+    for word in words:
+        i = 0
+        for i in range(len(acentos)):
+            if word == acentos[i]:
+                filtered.append(traducao[i])
+                continue
+        filtered.append(word)
+
+    return filtered
+
+
+def preprocess_msg(msg):
+
     ##RETURN A TUPLE LIKE -> (greetings,thanks,[vector of tokens],[pos_tag],extra_info (target))
-    nlp = spacy.load('C:\\Users\\Eduardo Vicente\\Anaconda3\\Lib\\site-packages\\pt_core_news_lg\\pt_core_news_lg-2.3.0')
+    nlp = spacy.load("pt_core_news_lg")
 
     #TOKENIZER -TOKENIZE WORDS/ELEMENTS IN THE MESSAGE STATMENT
     tokens = tokenize(msg)
     # print(tokens)
-    
+
     #FIND GREETINGS ->
     [greeting,idx_g] = find_greeting(tokens)
     if greeting == 1:
@@ -203,35 +160,26 @@ def Spacy_NLP_transform(msg):
     if thanks == 1:
         tokens.pop(idx_t)
     # REMOVE WORDS ^ INDEX (GREETING OR THANKS) -> WE NEED TO PASS THIS INFORMATION FOR THE MSG CONSTRUCTOR
-    
-   
+
     #REMOVE WHITESPACES AND PONCTUACTION ELEMENTS
     filtered = remove_noise(tokens)
-    
-    #REMOVE STOPWORDS 
+
+    #REMOVE STOPWORDS
     filtered = remove_stopwords(filtered)
-        
+
+    # TRADUZ ACENTOS
+    filtered = translate(filtered)
+
     #STEMMING & LEMMATIZATION
     filtered = stemming_lemmatization(filtered,nlp)
-    
+
     #PART-OF-SPEECH TAG POS
     pos_tag = part_of_speech(filtered,nlp)
-    print(pos_tag)
-    #BAG OF WORDS 
+    # print(pos_tag)
+    #BAG OF WORDS
     # bagwords = bag_of_words(filtered)
-        
+
     return [greeting,thanks,filtered,pos_tag]
-    
-
-def preprocess_msg(msg,type_nlp):
-    
-    if type_nlp:
-        
-        return NLPyPort_transform(msg)
-    else:
-        return Spacy_NLP_transform(msg)
-    # print(arr_tag)
-
 
 
 # preprocess_msg("Ola, estou a procura de uma camisola para a minha filha.")
